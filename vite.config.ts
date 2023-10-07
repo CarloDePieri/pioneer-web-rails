94e2d626e1033d8bbf1b1981053e9a6f70fe7bc0
Carlo De Pieri <depieri.carlo@gmail.com>
2023-10-07 12:06:29 +0200

    add pwa functionality

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   server: {
     open: true,
   },
