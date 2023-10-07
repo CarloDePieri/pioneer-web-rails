85ae2afd11543b21c362bca1d418a71a14f29b04
Carlo De Pieri <depieri.carlo@gmail.com>
2023-10-07 16:34:05 +0200

    change static image handling (now vite will actually serve them)

--- src/res/images/publicDomainImages.ts
+++ src/res/images/publicDomainImages.ts
@@ -1,41 +1,44 @@
 import { CardImageType } from "../images"
 
 export function publicDomainCards(): CardImageType {
-  let root = "src/res/images/publicDomain/"
+  let root = "/images/publicDomain/"
+
+  const url = (path: string) => root + path
+
   return {
     deck: {
       spades: {
-        ace: root + "deck/spades_ace.svg",
-        king: root + "deck/spades_king.svg",
-        queen: root + "deck/spades_queen.svg",
-        jack: root + "deck/spades_jack.svg",
-        ten: root + "deck/spades_ten.svg",
+        ace: url("deck/spades_ace.svg"),
+        king: url("deck/spades_king.svg"),
+        queen: url("deck/spades_queen.svg"),
+        jack: url("deck/spades_jack.svg"),
+        ten: url("deck/spades_ten.svg"),
       },
       hearts: {
-        ace: root + "deck/hearts_ace.svg",
-        king: root + "deck/hearts_king.svg",
-        queen: root + "deck/hearts_queen.svg",
-        jack: root + "deck/hearts_jack.svg",
-        ten: root + "deck/hearts_ten.svg",
+        ace: url("deck/hearts_ace.svg"),
+        king: url("deck/hearts_king.svg"),
+        queen: url("deck/hearts_queen.svg"),
+        jack: url("deck/hearts_jack.svg"),
+        ten: url("deck/hearts_ten.svg"),
       },
       diamonds: {
-        ace: root + "deck/diamonds_ace.svg",
-        king: root + "deck/diamonds_king.svg",
-        queen: root + "deck/diamonds_queen.svg",
-        jack: root + "deck/diamonds_jack.svg",
-        ten: root + "deck/diamonds_ten.svg",
+        ace: url("deck/diamonds_ace.svg"),
+        king: url("deck/diamonds_king.svg"),
+        queen: url("deck/diamonds_queen.svg"),
+        jack: url("deck/diamonds_jack.svg"),
+        ten: url("deck/diamonds_ten.svg"),
       },
       clubs: {
-        ace: root + "deck/clubs_ace.svg",
-        king: root + "deck/clubs_king.svg",
-        queen: root + "deck/clubs_queen.svg",
-        jack: root + "deck/clubs_jack.svg",
-        ten: root + "deck/clubs_ten.svg",
+        ace: url("deck/clubs_ace.svg"),
+        king: url("deck/clubs_king.svg"),
+        queen: url("deck/clubs_queen.svg"),
+        jack: url("deck/clubs_jack.svg"),
+        ten: url("deck/clubs_ten.svg"),
       },
     },
     jokers: {
-      black: root + "jokers/joker_black.svg",
-      red: root + "jokers/joker_red.svg",
+      black: url("jokers/joker_black.svg"),
+      red: url("jokers/joker_red.svg"),
     },
   }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
       devOptions: {
         enabled: true,
       },
