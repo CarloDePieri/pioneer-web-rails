5f402cccade9555d5b8e0e8b1719d036dbd8939f
Carlo De Pieri <depieri.carlo@gmail.com>
2023-10-07 11:34:10 +0200

    improve selected card effect

--- src/features/game/deck/PlayingCard.tsx
+++ src/features/game/deck/PlayingCard.tsx
@@ -1,9 +1,8 @@
-import { Box, Card } from "@mui/material"
+import { Card } from "@mui/material"
 import React, { PropsWithChildren } from "react"
 import { useAppDispatch, useAppSelector } from "../../../app/hooks"
 import { pick, selectPickedCard, unpick } from "../gameSlice"
 import { Card as GameCard } from "./gameDeck"
-import Paper from "@mui/material/Paper"
 import CardMedia from "@mui/material/CardMedia"
 
 interface Props extends PropsWithChildren<any> {
@@ -24,24 +23,25 @@ export function PlayingCard({ card }: Props) {
   }
 
   return (
-    <React.Fragment>
-      <Card
+    <Card
+      elevation={5}
+      sx={{
+        boxShadow: isSelected
+          ? "0px 3px 5px -1px rgba(25,118,210,0.4),0px 5px 8px 2px rgba(25,118,210,0.24),0px 1px 14px 4px rgba(25,118,210,0.7)"
+          : "",
+        width: {
+          xs: "50vw",
+          sm: "20vw",
+        },
+      }}
+    >
+      <CardMedia
         className={isSelected ? "pickedCard" : ""}
-        elevation={isSelected ? 1 : 5}
-        sx={{
-          width: {
-            xs: "50vw",
-            sm: "20vw",
-          },
-        }}
-      >
-        <CardMedia
-          component="img"
-          image={card.img}
-          alt={card.id}
-          onClick={togglePick}
-        />
-      </Card>
-    </React.Fragment>
+        component="img"
+        image={card.img}
+        alt={card.id}
+        onClick={togglePick}
+      />
+    </Card>
   )
 }
